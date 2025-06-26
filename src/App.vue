906f81940b175c09d55e04aefd66676ae29a27c6
HisseinHD <hisseindaoud6627@gmail.com>
2025-06-26 15:54:50 +0200

    c'est le premiere commit creation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
-  <WelcomeScreen />
+  <div id="app">
+    <WelcomeScreen 
+      v-if="currentScreen === 'welcome'" 
+      @proceed="currentScreen = 'phoneInput'"
+    />
+    <PhoneInput 
+      v-else-if="currentScreen === 'phoneInput'"
+      @submit="handlePhoneSubmit"
+      @back="currentScreen = 'welcome'"
+    />
+  </div>
 </template>
 
 <script>
-import WelcomeScreen from './components/MessageList.vue'
+import WelcomeScreen from './components/WelcomeScreen.vue'
+import PhoneInput from './components/PhoneInput.vue'
 
 export default {
   components: {
-    WelcomeScreen
+    WelcomeScreen,
+    PhoneInput
+  },
+  data() {
+    return {
+      currentScreen: 'welcome'
+    }
+  },
+  methods: {
+    handlePhoneSubmit(data) {
+      console.log('Numéro soumis:', data)
+      // ici vous pouvez envoyer à une API ou sauvegarder en localStorage
+    }
   }
 }
 </script>
@@ -16,5 +39,12 @@ export default {
 body {
   margin: 0;
   padding: 0;
+  font-family: Arial, sans-serif;
+}
+
+#app {
+  max-width: 100vw;
+  min-height: 100vh;
+  background-color: #f8f8f8;
 }
 </style>
