3a24dc957ceaf87a6e4924401c1b4d8f31d15f23
achtaA-a <sabounnhala@gmail.com>
2025-06-26 15:36:36 +0100

    verification code

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,13 @@
 <template>
-  <div id="app">
-    <WelcomeScreen 
-      v-if="currentScreen === 'welcome'" 
-      @proceed="currentScreen = 'phoneInput'"
-    />
-    <PhoneInput 
-      v-else-if="currentScreen === 'phoneInput'"
-      @submit="handlePhoneSubmit"
-      @back="currentScreen = 'welcome'"
-    />
-  </div>
+  <VerificationCode />
 </template>
 
 <script>
-import WelcomeScreen from './components/WelcomeScreen.vue'
-import PhoneInput from './components/PhoneInput.vue'
+import VerificationCode from './components/VerificationCode.vue'
 
 export default {
   components: {
-    WelcomeScreen,
-    PhoneInput
-  },
-  data() {
-    return {
-      currentScreen: 'welcome'
-    }
-  },
-  methods: {
-    handlePhoneSubmit(data) {
-      console.log('Numéro soumis:', data)
-      // ici vous pouvez envoyer à une API ou sauvegarder en localStorage
-    }
+    VerificationCode
   }
 }
 </script>
-
-<style>
-body {
-  margin: 0;
-  padding: 0;
-  font-family: Arial, sans-serif;
-}
-
-#app {
-  max-width: 100vw;
-  min-height: 100vh;
-  background-color: #f8f8f8;
-}
-</style>
