5b04b0b013d74f48fd36aa20e275086c9859ce6a
NadjlaSefadine <alzantoutienadjla@gmail.com>
2025-06-27 11:41:50 +0100

    vvv

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <VerificationCode />
+<contacts/>
+  <ProfileView />
 </template>
+
 
 <script>
 import VerificationCode from './components/VerificationCode.vue'
 
+import ProfileView from './components/ProfileView.vue';
+import contacts from './components/contacts.vue'; 
+
 export default {
+  name: 'ProfileView.vue',
   components: {
     VerificationCode
   }
+    ProfileView,
+    contacts
 }
+  };
 </script>
+
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+h1 {
+  color: black;
+}
+</style>
+
+
---
+++ src/components/SettingItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <i class="icon">{{ icon }}</i> {{ title }}
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'SettingItem',
+    props: {
+        title: {
+        type: String,
+        required: true
+        },
+        icon: {
+        type: String,
+        default: ''
+        }
+    }
+}
+</script>
